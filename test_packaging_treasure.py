7e1cbe9fa0c2f4c53dd02940edb37341cd974133
tugceoruc <tugceoruc14@gmail.com>
2019-11-06 16:16:31 +0000

    test update dungeon was added

--- test_packaging_treasure.py
+++ test_packaging_treasure.py
@@ -1,8 +1,21 @@
 # Unit tests for packaging_treasure.py
-
+import copy
 from packaging_treasure import outcome, update_dungeon, dungeon1
 ##
 #print(dungeon1)
 def test_outcome():
     #print (dungeon1)
     assert outcome(dungeon1)==-1
+
+
+def test_update_dungeon():
+    initial_dungeon=copy.deepcopy(dungeon1)
+    assert update_dungeon(dungeon1)!=initial_dungeon
+
+
+
+
+
+#def update_dungeon(dungeon):
+#    dungeon['adventurer']=random_move(dungeon['network'], dungeon['adventurer'])
+#    dungeon['troll']=random_move(dungeon['network'], dungeon['troll'])
