4865d5724553dd5945b6509c88331c50a0fa77e6
LeonardoCastro <lcastrogonzalez@turing.ac.uk>
2019-11-06 14:48:05 +0000

    Modify setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "packaging_treasure",
+    version = "0.1.0",
+    author = "L. Castro, K. Goldmann, T. Oruc, O. Sargoni"
+    packages = find_packages(exclude=['*test']),
+    install_requires = ['argparse'],
+    entry_points={
+        'console_scripts': [
+            'hunt = packaging_treasure.command:process'
+        ]})
