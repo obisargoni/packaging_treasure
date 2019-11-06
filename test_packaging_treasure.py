7067de3dcf8e3e20d72ac8b247a817a98d36e4ae
tugceoruc <tugceoruc14@gmail.com>
2019-11-06 15:04:20 +0000

    test for third function

--- test_packaging_treasure.py
+++ test_packaging_treasure.py
@@ -1,1 +1,8 @@
 # Unit tests for packaging_treasure.py
+
+from packaging_treasure import outcome, update_dungeon, dungeon1
+##
+#print(dungeon1)
+def test_outcome():
+    #print (dungeon1)
+    assert outcome(dungeon1)==-1
