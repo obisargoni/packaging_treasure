2bf85a6298416f42f3ff1d2b893dcfd6811e9224
LeonardoCastro <lcastrogonzalez@turing.ac.uk>
2019-11-06 14:58:08 +0000

    Added authors to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = "0.1.0",
     author = "L. Castro, K. Goldmann, T. Oruc, O. Sargoni"
     packages = find_packages(exclude=['*test']),
-    install_requires = ['argparse'],
+    install_requires = ['argparse', 'copy', 'random'],
     entry_points={
         'console_scripts': [
             'hunt = packaging_treasure.command:process'
